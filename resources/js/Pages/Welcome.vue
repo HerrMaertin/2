89bfc9b62d12b0143e12f41ed15e67292d02b37f
HerrMaertin <jan.maertin1010@gmail.com>
2023-08-04 09:43:01 +0200

    deploybranch folder content

---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,31 @@
+<!-- App.vue -->
+<template>
+    <div class="bg-gradient-to-r from-teal-400 via-teal-300 to-teal-400 min-h-screen">
+        <div class="container mx-auto py-10">
+            <h1 class="text-4xl font-bold text-white mb-6">SchwangerenYoga</h1>
+            <div class="grid grid-cols-2 gap-8">
+                <div>
+                    <!-- Hier kannst du ein Bild mit den Schmetterlingen einfügen -->
+                    <!-- Beispiel: <img resources="/path/to/butterflies.jpg" alt="Schmetterlinge" class="rounded-lg shadow-lg"> -->
+                    <img src="/path/to/yoga-image.jpg" alt="SchwangerenYoga" class="rounded-lg shadow-lg">
+                </div>
+                <div>
+                    <p class="text-white text-xl mb-4">
+                        Willkommen zu unserem SchwangerenYoga-Kurs! Wir bieten eine entspannende und sichere Umgebung, um Yoga während deiner Schwangerschaft zu praktizieren.
+                    </p>
+                    <p class="text-white text-lg mb-4">
+                        Unsere erfahrenen Lehrerinnen begleiten dich durch sanfte Yoga-Übungen, die auf die Bedürfnisse werdender Mütter zugeschnitten sind.
+                    </p>
+                    <p class="text-white text-lg">
+                        Buche noch heute deine erste Sitzung und tauche ein in die wunderbare Welt des SchwangerenYogas!
+                    </p>
+                    <button class="mt-6 bg-white text-gray-800 py-2 px-4 rounded-lg shadow-lg hover:bg-gray-300">Jetzt buchen</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
